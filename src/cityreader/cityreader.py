f78fbade4495c1cce8bc230d5ee43234bd3cd6d4
novinary <novina.dev@gmail.com>
2019-05-31 10:02:50 +0100

    MVP tests are passing for cityreader

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,3 +1,4 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 class City:
@@ -27,7 +28,14 @@ def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+    with open('cities.csv', newline='') as csv_file:
+      csv_reader = csv.reader(csv_file, delimiter=',')
+      line_count = 0
+      for row in csv_reader:
+        if line_count == 0:
+          line_count += 1
+        else:
+          cities.append(City(str(row[0]), float(row[3]), float(row[4])))
     return cities
 
 cityreader(cities)
